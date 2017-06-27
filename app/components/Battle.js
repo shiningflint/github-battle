e03168939c2c6e48e0c433a167a6d0e12ce9075a
Adam Christopher B Widjaja <adam.keristoper@gmail.com>
2017-06-27 14:57:00 +0900

    Dynamic rendering & Query params

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,33 @@
 var React = require('react');
 var PropTypes = require('prop-types');
+var Link = require('react-router-dom').Link;
+
+function PlayerPreview(props) {
+  return (
+    <div className='preview'>
+      <p>
+        <img
+          className='avatar-preview'
+          src={props.avatar}
+          alt={'Avatar for' + props.avatar}
+        />
+      </p>
+      <h2 className='username'>@{props.username}</h2>
+      <button
+        className='reset'
+        onClick={props.onReset.bind(null, props.id)}>
+        Reset
+      </button>
+    </div>
+  );
+}
+
+PlayerPreview.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  onReset: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired
+}
 
 class PlayerInput extends React.Component {
   constructor(props) {
@@ -71,6 +99,7 @@ class Battle extends React.Component {
     }
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleReset = this.handleReset.bind(this);
   }
 
   handleSubmit(id, username) {
@@ -82,20 +111,65 @@ class Battle extends React.Component {
     });
   }
 
+  handleReset(id) {
+    this.setState(() => {
+      var newState = {};
+      newState[id + 'Name'] = '';
+      newState[id + 'Image'] = null;
+      return newState;
+    });
+  }
+
   render() {
+    var match = this.props.match;
+    var playerOneName = this.state.playerOneName;
+    var playerOneImage = this.state.playerOneImage;
+    var playerTwoName = this.state.playerTwoName;
+    var playerTwoImage = this.state.playerTwoImage;
+
     return(
-      <div className='flex'>
-        {!this.state.playerOneName &&
-          <PlayerInput
-            id='playerOne'
-            label='Player One'
-            onSubmit={this.handleSubmit} />
-        }
-        {!this.state.playerTwoName &&
-          <PlayerInput
-            id='playerTwo'
-            label='Player Two'
-            onSubmit={this.handleSubmit} />
+      <div>
+        <div className='flex'>
+          {!playerOneName &&
+            <PlayerInput
+              id='playerOne'
+              label='Player One'
+              onSubmit={this.handleSubmit} />
+          }
+
+          {playerOneImage !== null &&
+            <PlayerPreview
+              avatar={playerOneImage}
+              username={playerOneName}
+              onReset={this.handleReset}
+              id='playerOne' />
+          }
+
+          {!playerTwoName &&
+            <PlayerInput
+              id='playerTwo'
+              label='Player Two'
+              onSubmit={this.handleSubmit} />
+          }
+
+          {playerTwoImage !== null &&
+            <PlayerPreview
+              avatar={playerTwoImage}
+              username={playerTwoName}
+              onReset={this.handleReset}
+              id='playerTwo' />
+          }
+        </div>
+        {playerOneImage && playerTwoImage &&
+          <div>
+            <Link className='button' to={{
+                pathname: match.url + '/results',
+                search: '?playerOneName=' + playerOneName + '&playerTwoName=' +
+                playerTwoName
+              }}>
+              Battle
+            </Link>
+          </div>
         }
       </div>
     )
